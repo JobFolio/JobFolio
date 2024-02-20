073c025479a7a1f75c55b63c2004c2cd8859f0c0
ay7991 <amy.yang7991@gmail.com>
2024-02-20 17:25:59 -0500

    add post question and get question middleware

---
+++ server/controllers/questionController.js
@@ -0,0 +1,11 @@
+const db = require('../models/dataModel');
+
+const questionController = {};
+
+questionController.postQuestion = (req, res, next) => {
+
+};
+
+questionController.getQuestion = (req, res, next) => {
+    
+};
