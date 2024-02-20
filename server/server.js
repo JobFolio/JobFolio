8e89c23588a89cce66a12cf0560524fd3490574c
ay7991 <amy.yang7991@gmail.com>
2024-02-20 18:59:31 -0500

    add job, user and question middleware to endpoints declaration

--- server/server.js
+++ server/server.js
@@ -19,33 +19,33 @@ const userController = require('./controllers/userController.js');
 const questionController = require('./controllers/questionController.js');
 
 // sign up user
-app.post('/signup', (req, res) => {
+app.post('/signup', userController.postUser, (req, res) => {
   res.status(200).send('user signed up');
 });
 
 // log in user
-app.get('/login', (req, res) => {
+app.get('/login', userController.getUser, (req, res) => {
   // redirect to main page occurs here
   res.status(200).send('user logged in');
 });
 
 // add job
-app.post('/addJob', (req, res) => {
+app.post('/addJob', jobController.postJob, (req, res) => {
   res.status(200).send('job added');
 });
 
 // get job
-app.get('/getJob', (req, res) => {
+app.get('/getJob', jobController.getJob, (req, res) => {
   res.status(200).send('jobs grabbed');
 });
 
 // add question
-app.post('/addQuestion', (req, res) => {
+app.post('/addQuestion', questionController.postQuestion, (req, res) => {
   res.status(200).send('questions added');
 });
 
 // get question
-app.get('/getQuestions', (req, res) => {
+app.get('/getQuestions', questionController.getQuestion, (req, res) => {
   res.status(200).send('questions retrieved');
 })
 
