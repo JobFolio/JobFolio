afe7b6b353f7f151f500091bf9d088fb3840204a
ay7991 <amy.yang7991@gmail.com>
2024-02-20 18:58:35 -0500

    mock username and password data in user controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,7 +3,11 @@ const db = require('../models/dataModel');
 const userController = {};
 
 userController.postUser = (req, res, next) => {
+    // mock user information
+    const username = 'erika_genshin';
+    const password = '123456';
 
+    
 };
 
 userController.getUser = (req, res, next) => {
