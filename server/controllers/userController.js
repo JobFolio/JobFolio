448c1933525dd053e600f61581e05d756ea44831
ay7991 <amy.yang7991@gmail.com>
2024-02-20 17:26:15 -0500

    add post user and get user middleware

---
+++ server/controllers/userController.js
@@ -0,0 +1,11 @@
+const db = require('../models/dataModel');
+
+const userController = {};
+
+userController.postUser = (req, res, next) => {
+
+};
+
+userController.getUser = (req, res, next) => {
+
+};
