8ea2192c690c8109bb90fb1eef62d3cdaa359b77
ay7991 <amy.yang7991@gmail.com>
2024-02-20 17:09:49 -0500

    add various endpoints in server file related to jobs, questions, and users

--- server/server.js
+++ server/server.js
@@ -13,6 +13,36 @@ app.use(express.urlencoded());
 app.use(express.static(path.resolve(__dirname, "../dist")));
 app.use(express.static(path.resolve(__dirname, "./src/static/images")));
 
+// sign up user
+app.post('/signup', (req, res) => {
+  res.status(200).send('user signed up');
+});
+
+// log in user
+app.get('/login', (req, res) => {
+  // redirect to main page occurs here
+  res.status(200).send('user logged in');
+});
+
+// add job
+app.post('/addJob', (req, res) => {
+  res.status(200).send('job added');
+});
+
+// get job
+app.get('/getJob', (req, res) => {
+  res.status(200).send('jobs grabbed');
+});
+
+// add question
+app.post('/addQuestion', (req, res) => {
+  res.status(200).send('questions added');
+});
+
+// get question
+app.get('/getQuestions', (req, res) => {
+  res.status(200).send('questions retrieved');
+})
 
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
